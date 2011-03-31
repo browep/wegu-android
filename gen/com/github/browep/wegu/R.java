5af7c2afc0e0b51f588e89304cb96faf0ad66b17
Paul <brower.paul@gmail.com>
2011-03-30 22:13:11 -0400

    icon change

--- gen/com/github/browep/wegu/R.java
+++ gen/com/github/browep/wegu/R.java
@@ -13,6 +13,8 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int two=0x7f020001;
+        public static final int wegu_512_512=0x7f020002;
+        public static final int wegu_72_72=0x7f020003;
     }
     public static final class id {
         public static final int alarm_off=0x7f050005;
